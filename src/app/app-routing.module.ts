0dbb0454644ab28bcf40321f375bc29a17834a16
Pablo <pablo_cesar1995@hotmail.com>
2022-01-11 22:35:35 -0500

    lazy loading in project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent,
+    path: 'auth',
+    loadChildren: () =>
+      import('@modules/auth/auth.module').then((m) => m.AuthModule),
   },
   {
-    path: 'users',
-    component: UsersComponent,
+    path: 'task',
+    loadChildren: () =>
+      import('@modules/task/task.module').then((m) => m.TaskModule),
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
 
 // modules groups components, services providers and import other modules
 
@@ -13,8 +11,6 @@ import { UsersComponent } from './users/users.component';
   // components, directives, pipes or filters just must be declared
   declarations: [
     AppComponent,
-    HomeComponent,
-    UsersComponent
   ],
   // modules just must be importeds
   imports: [ // all modules that this module will import
